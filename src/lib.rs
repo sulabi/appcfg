c387fde58a5234cc19e2329298a28d1aff9d7f49
Sulieman <168682205+sulabi@users.noreply.github.com>
2026-09-15 15:30:19 +0100

    some more docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,65 @@
+//! # appcfg
+//! A small lightweight filesystem config manager
+//! `appcfg` provides an api to load, deserialize and write
+//! config files for your application.
+//! This currently only supports the TOML format, however I shall
+//! introduce more formats in the future.
+//!
+//! ## Usage
+//!
+//! ```rust
+//! use appcfg::{Config, ConfigDirectory};
+//! use serde::{Deserialize, Serialize};
+//!
+//! #[derive(Debug, Serialize, Deserialize)]
+//! struct AppSettings {
+//!     port: u16,
+//!     verbose: bool
+//! }
+//! impl Default for AppSettings {
+//!     fn default() -> Self {
+//!         Self {
+//!             port: 9000,
+//!             verbose: true
+//!         }
+//!     }
+//! }
+//!
+//! fn main() -> Result<(), Box<dyn std::error::Error>> {
+//!     let config = Config::new(ConfigDirectory::Custom("./appconf".into()))?;
+//!     let settings = config.read_or_default::<AppSettings>()?;
+//!
+//!     if settings.verbose {
+//!         println!("using port: {}", settings.port);
+//!     }
+//!
+//!     Ok(())
+//! }
+//! ```
+//!
+//! ## Writing Config
+//!
+//! ```rust
+//! use appcfg::{Config, ConfigDirectory};
+//! use serde::Serialize;
+//!
+//! #[derive(Serialize)]
+//! struct AppSettings {
+//!     username: String
+//! }
+//!
+//! fn main() -> Result<(), Box<dyn std::error::Error>> {
+//!     let config = Config::new(ConfigDirectory::Custom("./appconf".into()))?;
+//!     let settings = AppSettings {
+//!         username: "jimmy".into()
+//!     };
+//!
+//!     config.write(&settings)?;
+//!
+//!     Ok(())
+//! }
+//! ```
+
 use std::{
     fs, io,
     path::PathBuf,
